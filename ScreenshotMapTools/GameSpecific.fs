0b24df8c60c98d2f4b9c0e975f5235bec4569f9e
brianmcn <lorgon@gmail.com>
2024-06-28 15:36:20 -0400

    prep refactoring for next game

---
+++ ScreenshotMapTools/GameSpecific.fs
@@ -0,0 +1,14 @@
+﻿module GameSpecific
+
+//let GAME = "Void Stranger"   // recompile for different uses, for now
+//let WINDOW_TITLE = "ScreenshotMapTools (Running) - Microsoft Visual Studio"  //"Void Stranger"
+let GAME = "Leafs Odyssey"   // recompile for different uses, for now
+let WINDOW_TITLE = "Leaf's Odyssey"
+let GAMESCREENW, GAMESCREENH = 960, 540
+let NATIVE_FACTOR = 2         // e.g. I am running it at 2x native pixel size
+let GAMENATIVEW, GAMENATIVEH = GAMESCREENW/NATIVE_FACTOR, GAMESCREENH/NATIVE_FACTOR
+let GAMEASPECT = float(GAMENATIVEW) / float(GAMENATIVEH)
+let VIEWX = 630   // multiple of 9, 7, 5 so that we can so various zooms well onscreen
+let MapArea  =  96, 14, 384, 256         // x,y,w,h
+let MetaArea = 230,  0, 120,  14
+
--- ScreenshotMapTools/Generic.fs
+++ ScreenshotMapTools/Generic.fs
@@ -44,21 +44,7 @@ let GetWindowScreenshot(hwnd:System.IntPtr, w, h) =
     gfxBmp.Dispose()
     bmp
 
-
-// root folder for a game:
-//let GAME = "Void Stranger"   // recompile for different uses, for now
-//let WINDOW_TITLE = "ScreenshotMapTools (Running) - Microsoft Visual Studio"  //"Void Stranger"
-let GAME = "Leafs Odyssey"   // recompile for different uses, for now
-let WINDOW_TITLE = "Leaf's Odyssey"
-let GAMESCREENW, GAMESCREENH = 960, 540
-let NATIVE_FACTOR = 2         // e.g. I am running it at 2x native pixel size
-let GAMENATIVEW, GAMENATIVEH = GAMESCREENW/NATIVE_FACTOR, GAMESCREENH/NATIVE_FACTOR
-let GAMEASPECT = float(GAMENATIVEW) / float(GAMENATIVEH)
-let VIEWX = 630   // multiple of 9, 7, 5 so that we can so various zooms well onscreen
-let MapArea  =  96, 14, 384, 256         // x,y,w,h
-let MetaArea = 230,  0, 120,  14
-
-
+open GameSpecific
 
 let GetRootFolder() = System.IO.Path.Combine(GAME)
 
